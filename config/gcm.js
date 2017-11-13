4592482e36226df1bc7e1b86d8b9cf59dd807bbf
vu.minh.tuan <FRAMGIA\vu.minh.tuan@framgia0278-pc.framgia.com>
2017-11-13 16:48:28 +0700

    create new announcement

--- config/gcm.js
+++ config/gcm.js
@@ -12,14 +12,14 @@ export const sendTopic = (data,topic,retrying) => {
                 'Authorization': `key=${process.env.SERVER_KEY}`
             },
             body: JSON.stringify({
-                "data" : data,
-                "to" : `/topics/${topic}`
+                'data' : data,
+                'to' : `/topics/${topic}`
             })
         },function(error,response,body){
             if(error) return reject(error);
             return resolve(body);
 
-        })
+        });
 
         // const gcmSender = new gcm.Sender(process.env.SERVER_KEY);
         // const message= new gcm.Message({data});
@@ -28,10 +28,10 @@ export const sendTopic = (data,topic,retrying) => {
         //     if(err) return reject(err);
         //     return resolve(response);
         // })
-    })
+    });
 };
 export const sendClass = (data,tokens,retrying) => {
-    console.log(tokens)
+    console.log(tokens);
     return new Promise((resolve,reject) =>{
         request({
             url: 'https://gcm-http.googleapis.com/gcm/send',
@@ -41,12 +41,12 @@ export const sendClass = (data,tokens,retrying) => {
                 'Authorization': `key=${process.env.SERVER_KEY}`
             },
             body: JSON.stringify({
-                "data" : data,
-                "registration_ids" : tokens
+                'data' : data,
+                'registration_ids' : tokens
             })
         },function(error,response,body){
             if(error) return reject(error);
             return resolve(body);
-        })
-    })
+        });
+    });
 };
